8f5aa0cfec344487c846dcdff80f639ebcd4e314
Juan Fco. Mange <juanfmange@gmail.com>
2023-04-10 00:44:59 -0600

    estilos globales con css en react

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './helloWorld';
 import { FirstComponent } from './firstComponent';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
